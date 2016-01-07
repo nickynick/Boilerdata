28d49a77253d2c0981e1f24d8398615699d9dc79
Nick Tymchenko <t.nick.a@gmail.com>
2016-01-07 06:40:53 +0300

    Umbrella header

--- Boilerdata/Boilerdata.h
+++ Boilerdata/Boilerdata.h
@@ -6,8 +6,47 @@
 //  Copyright © 2016 Pixty. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+// Data item
 
-@interface Boilerdata : NSObject
+#import "BLDataItem.h"
 
-@end
+
+// Data diff
+
+#import "BLDataDiff.h"
+#import "BLDataDiffChange.h"
+#import "BLMutableDataDiff.h"
+#import "BLMutableDataDiffChange.h"
+
+
+// Data provider protocols
+
+#import "BLDataProvider.h"
+#import "BLDataObserver.h"
+#import "BLDataEvent.h"
+#import "BLDataEventProcessor.h"
+
+
+// Abstract data providers
+
+#import "BLStaticDataProviderProxy.h"
+#import "BLAbstractDataProvider.h"
+#import "BLChainDataProvider.h"
+
+// Import these headers to build your own subclasses:
+/*
+#import <Boilerdata/BLStaticDataProviderProxy+Subclassing.h>
+#import <Boilerdata/BLAbstractDataProvider+Subclassing.h>
+#import <Boilerdata/BLChainDataProvider+Subclassing.h>
+ */
+
+
+// Actual data providers
+
+#import "BLArrayDataProvider.h"
+#import "BLFilterDataProvider.h"
+
+
+// Goodies
+
+#import "BLUtils.h"
