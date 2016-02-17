6284c261cbfec0da9fdb0cf504bea563f27f1400
Nick Tymchenko <t.nick.a@gmail.com>
2016-02-18 02:46:24 +0300

    Added demo project

---
+++ Demo/BoilerdataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BoilerdataDemo
+//
+//  Created by Nick Tymchenko on 18/02/16.
+//  Copyright © 2016 Nick Tymchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
